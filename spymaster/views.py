8e5306843e593c4b8ad48d48b99daaa4bd9d9dee
nehemiahnewell <nehemiahnewell@gmail.com>
2015-09-22 15:56:44 -0700

    added a create deck function

--- spymaster/views.py
+++ spymaster/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from random import  getrandbits, shuffle
 
 # Create your views here.
+
+def create_game_deck():
+    # init deck
+    deck = []
+
+    #get starting team
+    is_blue = bool(getrandbits(1))
+    if is_blue == 1:
+        BLUE = ['bl'] * 9
+        RED = ['rd'] * 8
+        BEIGE = ['bg'] *7
+        deck.extend(BLUE)
+        deck.extend(RED)
+        deck.extend(BEIGE)
+        deck.append('bk')
+    else:
+        BLUE = ['bl'] * 8
+        RED = ['rd'] * 9
+        BEIGE = ['bg'] *7
+        deck.extend(BLUE)
+        deck.extend(RED)
+        deck.extend(BEIGE)
+        deck.append('bk')
+    shuffle(deck)
+    return (is_blue,deck)
