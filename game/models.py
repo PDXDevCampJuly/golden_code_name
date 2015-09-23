22d82b5f7715bae9ba490f9f3fe37dbbb54f2c2d
grumps <max@ofmax.li>
2015-09-23 13:07:37 -0700

    added defaults to make migrations happy.

--- game/models.py
+++ game/models.py
@@ -7,20 +7,20 @@ import datetime
 # code_name Table
 
 class Team (models.Model):
-    is_blue = models.BooleanField(primary_key = True)
-    spies_left = models.PositiveSmallIntegerField()
+    is_blue = models.BooleanField(primary_key = True, default=True)
+    spies_left = models.PositiveSmallIntegerField(default=None)
 
 class Board(models.Model):
-    is_blue = models.BooleanField(primary_key = True)
-    guesses_allowed = models.PositiveSmallIntegerField()
+    is_blue = models.BooleanField(primary_key = True, default=True)
+    guesses_allowed = models.PositiveSmallIntegerField(default=0)
 
 class Clue(models.Model):
-    clue = models.CharField(max_length = 50)
-    num_of_cards = models.PositiveSmallIntegerField()
+    clue = models.CharField(max_length = 50, default=None)
+    num_of_cards = models.PositiveSmallIntegerField(default=None)
     team = models.ForeignKey(Team)
 
 class Card(models.Model):
-    word = models.CharField(max_length = 50)
+    word = models.CharField(max_length = 50, default=None)
     selected = models.BooleanField(default = False)
     BLUE = 'bl'
     RED = 'rd'
