3c49555cc43b3af8098680d5fc15971b060acc0e
grumps <max@ofmax.li>
2015-09-23 14:03:36 -0700

    wired in index page, without view.

--- code_name/settings.py
+++ code_name/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'code_name.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 ##################
 # LOCAL SETTINGS #
 ##################
