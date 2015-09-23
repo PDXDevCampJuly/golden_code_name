b9cacef67a4449ce87e4d1d5e4825aa832cc1e65
grumps <max@ofmax.li>
2015-09-23 16:03:24 -0700

    working listview.

--- spy/views.py
+++ spy/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from game.models import Board, Card, Team
 
 # Create your views here.
+
+class GameBoardList(ListView):
+    """
+    Implement Generic ListView
+    """
+    model = Card
+    template_name = 'spy.html'
+    def get_context_data(self, **kwargs):
+        """
+        Implement our custom context data
+        """
+        context = super(GameBoardList, self).get_context_data(**kwargs)
+        context['game_status']= Board.objects.get()
+        return context
+
+
+
+
+
