84007fbc4635758ddca49622faedd8d3f29baef3
nehemiahnewell <nehemiahnewell@gmail.com>
2015-09-23 10:58:57 -0700

    moved from spymaster to game, added the rest of the start game logic

--- spymaster/views.py
+++ spymaster/views.py
@@ -1,34 +1,4 @@
 from django.shortcuts import render
-from random import  getrandbits, shuffle
-from game.models import *
 
 # Create your views here.
 
-def create_game_deck():
-    # init deck
-    deck = []
-
-    #get starting team
-    is_blue = bool(getrandbits(1))
-    if is_blue == 1:
-        BLUE = ['bl'] * 9
-        RED = ['rd'] * 8
-        BEIGE = ['bg'] *7
-        deck.extend(BLUE)
-        deck.extend(RED)
-        deck.extend(BEIGE)
-        deck.append('bk')
-    else:
-        BLUE = ['bl'] * 8
-        RED = ['rd'] * 9
-        BEIGE = ['bg'] *7
-        deck.extend(BLUE)
-        deck.extend(RED)
-        deck.extend(BEIGE)
-        deck.append('bk')
-    shuffle(deck)
-    Card.objects.all().delete()
-    for card in deck:
-        person = Card(word='foo',card_type=card)
-        person.save()
-    Board.is_blue = is_blue
