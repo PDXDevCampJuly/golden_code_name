58c65dc4c811c396762e1391ccc4920d8f310f42
grumps <max@ofmax.li>
2015-09-23 16:35:19 -0700

    remove fields that aren't needed,

--- spy/views.py
+++ spy/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from game.models import Board, Card, Team
+from game.models import Board, Card, Clue
 
 # Create your views here.
 
@@ -8,14 +8,16 @@ class GameBoardList(ListView):
     """
     Implement Generic ListView
     """
-    model = Card
+    queryset = Card.objects.all().only('word', 'id', 'selected')
     template_name = 'spy.html'
     def get_context_data(self, **kwargs):
         """
         Implement our custom context data
         """
         context = super(GameBoardList, self).get_context_data(**kwargs)
-        context['game_status']= Board.objects.get()
+        context['game_status'] = Board.objects.get()
+        # TODO once we have working clue data
+        # context['clue'] = Clue.objects.get().last()
         return context
 
 
