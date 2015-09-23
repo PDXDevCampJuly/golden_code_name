9f8fe57b6f32589bc3dea83869ede8387de350e1
grumps <max@ofmax.li>
2015-09-23 14:05:03 -0700

    remove spy ref.

--- code_name/urls.py
+++ code_name/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'code_name.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', include('game.urls')),
-    url(r'^spy/', include('spy.urls')),
+    url(r'^$', include('game.urls'))
+    # url(r'^spy/', include('spy.urls')),
 ]
