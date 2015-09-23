403a884e2b104caa12a3656af100c6afba1689be
nehemiahnewell <nehemiahnewell@gmail.com>
2015-09-23 15:27:37 -0700

    set up hooks

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
+import game.views
 from django.views.generic import TemplateView
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'))
+    url(r'^$',game.views.start_index_page),
 ]
