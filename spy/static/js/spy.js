2bfc8937d08b1d5aa3980627a4cad2f8ee9e3710
williamsmichael <willia32@hotmail.com>
2015-09-24 13:30:50 -0700

    projects game status

---
+++ spy/static/js/spy.js
@@ -0,0 +1,7 @@
+window.alert("sometext");
+// var $player_turn_status = $("#game_status");
+// if ($player_turn_status.attr('class') === "True") {
+//   $(".colmask").css('background-color', 'red');
+// } else {
+//   $(".colmask").css('background-color', 'red');
+// }
