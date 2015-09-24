f965ce9d1b4f9ab0565a4c761b77f64dde3fe949
williamsmichael <willia32@hotmail.com>
2015-09-24 15:47:53 -0700

    runserver working, dictionary populating

--- spy/static/js/spy.js
+++ spy/static/js/spy.js
@@ -1,7 +1,7 @@
-window.alert("sometext");
-// var $player_turn_status = $("#game_status");
-// if ($player_turn_status.attr('class') === "True") {
-//   $(".colmask").css('background-color', 'red');
-// } else {
-//   $(".colmask").css('background-color', 'red');
-// }
+// window.alert("sometext");
+var $player_turn_status = $("#game_status");
+if ($player_turn_status.attr('class') === "True") {
+  $(".colmask").css('background-color', '#6699CC');
+} else {
+  $(".colmask").css('background-color', '#FF6666');
+}
