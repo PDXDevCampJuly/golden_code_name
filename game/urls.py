b4b8cc3786ec0088495eb8289a4c0d9ee52dfc89
williamsmichael <willia32@hotmail.com>
2015-09-26 21:27:59 -0700

    adjusts base styles and layout for updated database

--- game/urls.py
+++ game/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 import game.views
 import spymaster.views
 from django.views.generic import TemplateView
+
 urlpatterns = [
     url(r'^$', game.views.start_index_page),
     url(r'^select/', game.views.select_player, name='select_player'),
